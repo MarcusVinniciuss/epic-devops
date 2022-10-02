49d7159bb89eb274e5b5ef98fc15b376875657bb
Marcus Vinnicius <86576184+MarcusVinniciuss@users.noreply.github.com>
2022-10-02 20:31:45 -0300

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+## Integrantes
+
+* André Hugo Bastos da Silva - 88588
+* Helouíse Cristina de Almeida Itokazo - 85110
+* Marcus Vinnicius Carvalho dos Santos - 88469
+* Matheus Ferreira Santana - 88241
+* Oswaldo Gomes Moreira - 88526
---
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/config/security/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package br.com.fiap.epictaskapi.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.httpBasic()
+            .and()
+            .authorizeHttpRequests()
+                // tarefas
+                .antMatchers(HttpMethod.GET, "/api/task/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/task").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/api/task/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/api/task/**").permitAll()
+
+                //usu�rios
+                .antMatchers(HttpMethod.GET, "/api/user/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/user").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/api/user/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/api/user/**").permitAll()
+
+                //h2-console
+                .antMatchers("/h2-console/**").permitAll()
+
+                .anyRequest().denyAll()
+            .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .headers().frameOptions().disable()
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
